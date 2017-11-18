5fc1baf3573a37e915949af6dce1f96fc3f7f8da
gang.lvg <gang.lvg@alibaba-inc.com>
2017-11-18 13:42:22 +0800

    script that install docker in ubuntu 1604

---
+++ kubernetes-in-action/common/get_docker_1604.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## prepare
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+## install
+sudo apt-get update -y
+sudo apt-get install -y docker-ce
